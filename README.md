6b32d9443af00e2143468f3a96b8309b2ad8ed3a
kihong08 <kihong08@gmail.com>
2016-10-26 15:01:03 +0900

    th_filter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # holography_nr
 personal project - noise reduction of complex hologram
+---
+## 2016.10.26
+* threshold filter looks nice, unexpectedly
+* 500 um of circular filter is still okay
--- reconstruction_nr.m
+++ reconstruction_nr.m
@@ -1,40 +1,35 @@
 % RECONSTRUCTION WITH NOISE REDUCION
 % hologram noise reduction by frequency histogram apodization
-clear; clc;
+clear; clc; close all;
 
 rD = 70e-3;
 lambda = 635e-9;
+pp = 4.54e-6;
+nfreq = 1/(2*sqrt(2)*pp);
+gamma = 0.25;
     
     %createCH;
     openCH;
     res = size(CH, 2);
     
-imshowpair(CH_real, CH_imag, 'montage');
-title('complex hologram');
+% imshowpair(CH_real, CH_imag, 'montage');
+% title('complex hologram');
 
 fCH = fftshift(fft2(fftshift(CH)));
 fCH_re = real(fCH);
 fCH_im = imag(fCH);
-figure;
-imshowpair(fCH_re, fCH_im, 'montage');
-title('complex hologram - F.T.');
+% figure;
+% imshowpair(fCH_re, fCH_im, 'montage');
+% title('complex hologram - F.T.');
 
-% method 1 - spatial filter
-c = linspace(-res/2, res/2, res); r = c;
-[C, R] = meshgrid(c, r);
-spFilter = sqrt(C.^2 + R.^2);
-spFilter(spFilter > 100) = 0;
-spFilter(spFilter ~= 0) = 1;
+% create mesh
+r = linspace(-pp*res/2, pp*res/2, res);  
+c = r;
+[C, R] = meshgrid(r, c);
 
-fCH_filtered = fCH * spFilter;
-CH_filtered = fftshift(fft2(fftshift(fCH_filtered)));
 
-figure;
-imshowpair(CH_imag, imag(CH_filtered), 'montage')
-title('CH - before, and after filtered');
-
-% reconstruction
-A1 = fCH_filtered;
+% conventional reconstruction
+A1 = fCH;
 p = exp((1j*pi/(lambda*rD)) .* (R.^2 + C.^2));     % fresnel back propagation 
 p = fftshift(fft2(fftshift(p)));
 antialiased = (1/lambda) * (sqrt(R.^2 + C.^2)/rD);
@@ -42,3 +37,42 @@ pupil = antialiased < nfreq;
 p = p .* pupil;
 Az1 = A1.*p;
 EI = fftshift(fft2(fftshift(Az1)));
+EI1 = EI .* conj(EI);
+EI1 = EI1.^gamma;
+imwrite(uint8(255.*EI1./max(max(EI1))), 'img_original.jpg');
+
+% Q(s) generation
+p = exp((1j*pi/(lambda*rD)) .* (R.^2 + C.^2));     % fresnel back propagation 
+p = fftshift(fft2(fftshift(p)));
+antialiased = (1/lambda) * (sqrt(R.^2 + C.^2)/rD);
+pupil = antialiased < nfreq;
+p = p .* pupil;
+
+filterRadius = 0:0.25e-3:5e-3;
+for i = filterRadius
+    % spatial filter generation
+    spFilter = sqrt(C.^2 + R.^2);
+    spFilter(spFilter > i) = 0;
+    spFilter = spFilter./max(max(spFilter));
+%     spFilter(spFilter ~= 0) = 1;
+    %figure; imshow(spFilter);
+
+    fCH_filtered = fCH .* spFilter;
+
+
+
+    A1 = fCH_filtered;
+
+    Az1 = A1.*p;
+    EI = fftshift(fft2(fftshift(Az1)));
+    EI2 = EI .* conj(EI);
+    EI2 = EI2.^gamma;
+    EI2 = 255 .* EI2 ./ max(max(EI2));
+    imwrite(uint8(EI2), sprintf('img_invCircFilter_%d.jpg', i));
+end
+
+figure;
+imshowpair(EI1.^gamma, EI2.^gamma, 'montage');
+title('reconstructed - before and after filtered, \gamma = 0.25')
+
+% method2 - spatial frequency apodization
